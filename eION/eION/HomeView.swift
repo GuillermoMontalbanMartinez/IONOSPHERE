707b84d23956d2cccf7d86d2e77baae0f0f02d98
Marta Fernandez Garcia <mfg306@inlumine.ual.es>
2021-11-23 15:50:11 +0100

    Incorporando las vista inicial, la vista del pulso, la fila de las tablas y el buscador

---
+++ eION/eION/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  eION
+//
+//  Created by Marta Fernandez Garcia on 23/11/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image("AppLogo").resizable().frame(width: 200, height: 200, alignment: .center)
+                }.frame(width: geometry.size.width, height: geometry.size.height/2)
+
+                Text("Bienvenido de nuevo, usuario").font(.title)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
