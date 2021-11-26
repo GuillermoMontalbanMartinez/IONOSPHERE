2c35dab47d484b9d23d9caf8025b69be8f78f5fb
Marta Fernandez Garcia <mfg306@inlumine.ual.es>
2021-11-26 18:41:24 +0100

    Arreglando detalles vista MicrofonoView

--- eION/eION/CrearPulsoView.swift
+++ eION/eION/CrearPulsoView.swift
@@ -13,76 +13,72 @@ struct CrearPulsoView: View {
     @State var claseSeleccionada = ""
     @State var valorSlider : Double = 0
     @State var modoGrabacion : Bool = false
-   
     
     var body: some View {
         NavigationView{
-        VStack{
             VStack{
-                Section{
+                VStack{
+                    Section{
                         VStack{
                             Spacer()
                             Text("Introduzca un valor para A03: \((round(100000 * valorSlider) / 100000))").padding(.top)
                             Slider(value: $valorSlider, in: -1...1).frame(width:300)
                             Spacer()
                             
-                                
-                                HStack{
-                                    Text("Introduzca un valor para A27: \(valorPorVoz) ")
-                                            Spacer()
-                                    Image(systemName: "mic.fill").resizable().frame(width:25, height: 32).onTapGesture {
-                                        modoGrabacion.toggle()
-                                    }.sheet(isPresented: $modoGrabacion ){
-                                        MicrofonoView(valorPorVoz: $valorPorVoz,modoGrabacion: $modoGrabacion)
-                                    }
-                                 
-                                }.frame(width: 300, alignment: .leading)
-                          
                             
-                                           
-                                HStack{
-                                    NavigationView {
-                                               Form {
-                                                   Section {
-                                                       Picker("Seleccionar clase", selection: $claseSeleccionada) {
-                                                           ForEach(clases, id: \.self) {
-                                                               Text($0)
-                                                    }
+                            HStack{
+                                Text("Introduzca un valor para A27: \(valorPorVoz) ")
+                                Spacer()
+                                Image(systemName: "mic.fill").resizable().frame(width:25, height: 32).onTapGesture {
+                                    modoGrabacion.toggle()
+                                }.sheet(isPresented: $modoGrabacion ){
+                                    MicrofonoView(valorPorVoz: $valorPorVoz,modoGrabacion: $modoGrabacion)
+                                }
+                            }.frame(width: 300, alignment: .leading)
+                            
+                            HStack{
+                                NavigationView {
+                                    Form {
+                                        Section {
+                                            Picker("Seleccionar clase", selection: $claseSeleccionada) {
+                                                ForEach(clases, id: \.self) {
+                                                    Text($0)
                                                 }
                                             }
                                         }
+                                    }
                                 }
-                        }
+                            }
                             
-                        VStack{
-                        Button(action:{
-                            print("Pulso creado")
-                            let valorSliderRedondeado  = String(String(valorSlider).prefix(7))
-                            
-                            valorSlider = Double(valorSliderRedondeado) ?? 0.0
-                        }, label:{
-                            Text("Crear pulso")
-                            
-                        }).frame(width: 150, height: 50 ).foregroundColor(.white).background(Color.accentColor).cornerRadius(10).padding()
-                       
-                        }.frame(width : 265)
-                  }.listRowSeparator(.hidden)
-              
-                
-            }.frame(width: 350, height:450 ).background(.white).cornerRadius(30).shadow( radius: 10).onAppear(perform: {
-                UITableView.appearance().backgroundColor = .clear
-                
-            })
-           
-        }.navigationTitle(Text("Nuevo pulso").font(.largeTitle).foregroundColor(Color.accentColor))
+                            VStack{
+                                Button(action:{
+                                    print("Pulso creado")
+                                    let valorSliderRedondeado  = String(String(valorSlider).prefix(7))
+                                    
+                                    valorSlider = Double(valorSliderRedondeado) ?? 0.0
+                                }, label:{
+                                    Text("Crear pulso")
+                                    
+                                }).frame(width: 150, height: 50 ).foregroundColor(.white).background(Color.accentColor).cornerRadius(10).padding()
+                                
+                            }.frame(width : 265)
+                        }.listRowSeparator(.hidden)
+                        
+                        
+                    }.frame(width: 350, height:450 ).background(.white).cornerRadius(30).shadow( radius: 10).onAppear(perform: {
+                        UITableView.appearance().backgroundColor = .clear
+                        
+                    })
+                    
+                }.navigationTitle(Text("Nuevo pulso"))
+            }
         }
     }
-}
-
-struct CrearPulsoView_Previews: PreviewProvider {
-    static var previews: some View {
-        CrearPulsoView()
-        }
-    }
+    
+    struct CrearPulsoView_Previews: PreviewProvider {
+     static var previews: some View {
+     CrearPulsoView()
+     }
+     }
 }
 
--- eION/eION/MicrofonoView.swift
+++ eION/eION/MicrofonoView.swift
@@ -12,46 +12,46 @@ struct MicrofonoView: View {
     @Binding var valorPorVoz :String
     @State var isRecording : Bool = false
     @Binding var modoGrabacion: Bool
+
     var body: some View {
         
         VStack {
             Spacer()
             if(!isRecording){
                 VStack{
-            Image(systemName: "mic.circle").resizable().frame(width: 150, height: 150, alignment: .center).foregroundColor(Color.accentColor).onTapGesture {
-                isRecording.toggle()
-            }
-                Text("Pulsa para grabar").padding().font(.footnote)
-                }.onAppear{
-                    speechRecognizer.stopRecording()
+                    Image(systemName: "mic.circle").resizable().frame(width: 150, height: 150, alignment: .center).foregroundColor(Color.accentColor).onTapGesture {
+                        isRecording.toggle()
+                    }
+                    Text("Pulsa para grabar").padding().font(.footnote)
                 }
-            }else{
+            } else{
                 VStack{
-                Image(systemName: "waveform.and.mic").resizable().frame(width: 150, height: 150, alignment: .center).foregroundColor(Color.accentColor)
+                    Image(systemName: "waveform.and.mic").resizable().frame(width: 150, height: 150, alignment: .center).foregroundColor(Color.accentColor)
                     if(valorPorVoz.count == 0){
                         Text("Hable para introducir el valor A27").padding()
                     }else{
-                        
                         Text("Valor de A27: \(valorPorVoz)").padding()
                     }
+                    Button(action:{
+                        
+                        isRecording.toggle()
+                    }, label:{
+                        Image(systemName: "x.circle").resizable().frame(width: 40, height: 40, alignment: .center).foregroundColor(Color.black)
+                        
+                    })
                 }.onAppear{
                     speechRecognizer.record(to: $valorPorVoz)
                 }
             }
-            Spacer()
             
-          
-            Spacer()
-            Button(action:{
-                
-                isRecording.toggle()
-            }, label:{
-                Image(systemName: "x.circle").resizable().frame(width: 40, height: 40, alignment: .center).foregroundColor(Color.black)
-                
-            })
+            Button() {
+                modoGrabacion.toggle()
+            } label: {
+                Text("Cancelar")
+            }.padding().background(Color.accentColor).foregroundColor(.white).clipShape(Capsule())
             
+            Spacer()
         }
-        
     }
 }
 
