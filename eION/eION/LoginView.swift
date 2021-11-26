c81af76a0e784cef762dd2656a1e10b53deecb6b
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2021-11-26 21:06:16 +0100

    mejora del codigo

--- eION/eION/LoginView.swift
+++ eION/eION/LoginView.swift
@@ -12,56 +12,83 @@ struct LoginView: View {
     @State var password: String = ""
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack(alignment: .center, spacing: -140) {
-                Image("AppLogo").resizable().frame(width: 200, height: 200, alignment: .center)
-                VStack(alignment: .center, spacing:20) {
-                    HStack {
-                        Text("Nombre      ")
-                        TextField("Nombre usuario", text: $username)
-                                .padding()
-                                .cornerRadius(5.0)
-                                .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray, lineWidth: 2))
-                        }.frame(width: 325, height: 70, alignment: .center)
-                        
-                    HStack {
-                        Text("Contraseña")
-                        TextField("Contraseña", text: $password)
-                            .padding()
-                            .cornerRadius(5.0)
-                            .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.gray, lineWidth: 2))
-                            .padding(.bottom,20)
-                    }.frame(width: 325, height: 70, alignment: .center)
-                        
-                    Button( action: {
-                        //iniciarSesion(nombre: username, contraseña: password)
-                        print("Hello world \(username)")
-                        
-                    }, label: {
-                        Text("Iniciar sesión")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color(red: 13.0/255.0, green: 16.0/255.0, blue: 51.0/255.0))
-                            .cornerRadius(13)
-                            .shadow(radius: 16)
-                            .frame(width: 250, height: 90, alignment: .center)
-                    })
-                }.frame(width: geometry.size.width/1.1, height: geometry.size.height/2, alignment: .center)
-                    .background(Color.white)
-                    .cornerRadius(30)
-                    .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
-                    .shadow(radius: 10)
-               
-                Button(action: {
-                    
-                }, label : {
-                    Text("¿No tienes cuenta?")
-                    Text("Registrate").underline()
-                }).frame(width: 400, height: 200, alignment: .center)
+        NavigationView {
+            ZStack {
+                VStack {
+                    Image("AppLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200)
+                        .clipShape(Circle())
+                        .frame( alignment: .top)
+                    Spacer()
+                }
+                
+                VStack(spacing: 15) {
+                    LoginForm()
+                    HStack() {
+                        Text("¿No tienes cuenta?", tableName: "Login")
+                         Button {
+                             
+                         } label: {
+                             Text("Registrate", tableName: "Login")
+                                .foregroundColor(.black)
+                                .underline()
+                        }
+                    }
+                }
             }
         }
+    }
+}
+
+private struct LoginForm: View {
+    
+    @State var username:String = ""
+    @State var password:String = ""
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 30) {
+                HStack(alignment: .bottom) {
+                    Text("Nombre", tableName: "Login")
+                        .frame(minWidth: 90, alignment: .leading)
+                        .disableAutocorrection(true)
+                    VStack(spacing: 0) {
+                        TextField("", text: $username)
+                        Divider()
+                            .background(.black)
+                    }
+                }
+            
+                HStack(alignment: .bottom) {
+                    Text("Contraseña", tableName: password)
+                        .frame(minWidth: 90, alignment: .leading)
+                    VStack(spacing: 0) {
+                        SecureField("", text: $password)
+                        Divider()
+                            .background(.black)
+                    }
+                }
+   
+            }.padding([.leading, .trailing], 20)
+             .padding([.top, .bottom], 30)
+            
+            Button {
+                            
+            } label: {
+                Text("Iniciar sesión", tableName: "Login")
+                    .foregroundColor(.white)
+                    .padding([.top, .bottom], 15)
+                    .padding([.leading, .trailing], 25)
+                    .background(.black)
+                    .cornerRadius(8)
+                    .shadow(color: Color(red: 13.0/255.0, green: 16.0/255.0, blue: 51.0/255.0).opacity(0.4), radius: 1, y: 4)
+                }.padding(.bottom, 30)
+        }.background()
+         .cornerRadius(20)
+         .shadow(color: .gray.opacity(0.4), radius: 4)
+         .padding([.leading, .trailing])
     }
 }
 
